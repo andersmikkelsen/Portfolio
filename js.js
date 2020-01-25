a67548af3a2fa6dae7c94efcaed16049d143798d
andersmikkelsen <anders.s.mikkelsen@gmail.com>
2020-01-25 18:11:19 +0100

    Add portfolio

---
+++ js.js
@@ -0,0 +1,7 @@
+  $(document).ready(function(){
+$('#row-one').fadeIn(1000);
+$('#row-two').fadeIn(2000);
+$('#row-three').fadeIn(3000);
+$('#row-four').fadeIn(4000);
+$('#row-five').fadeIn(5000);
+});
